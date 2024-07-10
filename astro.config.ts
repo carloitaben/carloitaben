4bf0ddf0bc7b11d799a6353bcda9756237005ea5
Carlo <hello@carlo.works>
2024-07-10 21:01:39 +0200

    feat: updates everything

---
+++ astro.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "astro/config"
+import tailwindcss from "tailwindcss"
+import autoprefixer from "autoprefixer"
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    css: {
+      postcss: {
+        plugins: [tailwindcss, autoprefixer],
+      },
+    },
+  },
+})
