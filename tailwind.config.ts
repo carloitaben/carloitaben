e351baf9e622980e0d253f70c1322eb2d591a56b
Carlo <hello@carlo.works>
2024-07-12 16:13:33 +0200

    feat: some content

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,9 @@ import type { Config } from "tailwindcss"
 
 export default {
   content: ["./src/**/*.{ts,tsx,astro}"],
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
   theme: {
     extend: {},
   },
