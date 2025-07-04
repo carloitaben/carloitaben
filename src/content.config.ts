67d4e13a3b559adf770e55b84c46dc99b1a187bf
Carlo <hola.carlodominguez@gmail.com>
2025-07-04 20:53:27 +0200

    chore: remove requirement for pending slash on blog slugs

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,7 +4,7 @@ import { glob } from "astro/loaders"
 const blog = defineCollection({
   loader: glob({ pattern: "**/*.md", base: "./src/blog" }),
   schema: z.object({
-    slug: z.string().startsWith("/"),
+    slug: z.string(),
     year: z.union([z.number(), z.literal("evergreen")]),
     title: z.string(),
   }),
