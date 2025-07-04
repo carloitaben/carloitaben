0492929b5b85c92441d9ed75e3afa582f6f65ddc
Carlo <hola.carlodominguez@gmail.com>
2025-07-04 17:09:06 +0200

    feat: add basic content collections config

---
+++ src/content.config.ts
@@ -0,0 +1,12 @@
+import { defineCollection, z } from "astro:content"
+import { glob, file } from "astro/loaders"
+
+const blog = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/blog" }),
+  schema: z.object({
+    draft: z.boolean(),
+    title: z.string(),
+  }),
+})
+
+export const collections = { blog }
