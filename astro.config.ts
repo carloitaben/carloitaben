2d0f36ad255841b1fbee02f7425f03ea4bffa8b6
Carlo <hello@carlo.works>
2026-09-04 01:05:22 +0200

    feat: back to triangle company

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from "astro/config"
-import aws from "astro-sst"
 import tailwindcss from "tailwindcss"
 import autoprefixer from "autoprefixer"
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: aws(),
   server: {
     port: 3000,
   },
